f8258e449d9e13ec61391b759b7179dc8799569f
liuyuchan286 <liuyuchan286@gmail.com>
2021-07-29 11:45:32 +0800

    币安的apidoc封装

---
+++ src/test/java/com/binance/binance/BinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binance.binance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
